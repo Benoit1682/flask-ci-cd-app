5b5b37c90805e2c427f8fbd66326b09b8a90ad0a
Benoit1682 <benfournier01gmail.com>
2025-05-15 21:35:37 +0200

    Second Change Welcome Message

--- flaskappbfo.py
+++ flaskappbfo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps : 1ère modification !"
+    return "Hello, DevOps : Preuve vidéo modifications"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
