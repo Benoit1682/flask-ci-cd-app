eb41ebe87145cedc77d7f17a533a11f0066afa42
Yuen001 <marco.moreira07@hotmail.com>
2025-05-16 22:06:46 +0200

    modif code benoit

--- flaskappbfo.py
+++ flaskappbfo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps : Preuve vidéo modifications"
+    return "Nagi en mode dark sasuke"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
